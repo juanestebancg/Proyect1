5d982eba57d9dddd567bbe3e5c7b5c306c0d3ff9
juanestebancg <juanestebancg@javerianacali.edu.co>
2017-05-04 16:44:36 -0500

    version 1
    dd

---
+++ ventanaproyecto.py
@@ -0,0 +1,81 @@
+import tkinter
+
+w0=tkinter.Tk()# se crea la ventana
+
+w0.geometry("700x500")#tamaño de la ventana
+
+fondo=tkinter.PhotoImage(file="vector.png")#fondo de la pantalla
+
+lblfondo=tkinter.Label(w0,image=fondo).place(x=0)#Label del fondo
+
+#campo de entrada label jugador 1
+lblfondo=tkinter.Label(w0,text="Jugador 1: ",font=("Arial",14)).place(x=10,y=10)
+
+#jugador 1 campo
+
+j1=tkinter.StringVar()
+j1.set("")
+j1txt=tkinter.Entry(w0,textvariable=j1).place(x=120,y=10)
+
+#campo de entrada label jugador 2
+lblfondo=tkinter.Label(w0,text="Jugador 2: ",font=("Arial",14)).place(x=10,y=40)
+
+#jugador 1 campo
+
+j2=tkinter.StringVar()
+j2.set("")
+j2txt=tkinter.Entry(w0,textvariable=j2).place(x=120,y=40)
+
+global c1,nvl1,mapa,lblmapa
+
+def nivel1(): # Abrir la ventana del nivel 1, fusionar funcion de guardado a precionar nivel 1
+    """
+    Esta funcion es para abrir una nueva ventana, que abre el mapa del nivel 1
+    """
+    nvl1 = tkinter.Tk()#crear la ventana del nivel 1
+    nvl1.geometry("800x850")#dimensiones de la ventana
+    c1=tkinter.Canvas(nvl1, width=200, height=100).pack
+    mapa=tkinter.PhotoImage(file="1nivel.png")#imagen del mapa
+    lblmapa=tkinter.Label(nvl1,image=mapa)
+def nivel2(): # Abrir la ventana del nivel 2, fusionar funcion de guardado a precionar nivel 2
+    """
+    Esta funcion es para abrir una nueva ventana, que abre el mapa del nivel 2
+    """
+    nvl2 = tkinter.Tk()
+
+def nivel3(): # Abrir la ventana del nivel 3, fusionar funcion de guardado a precionar nivel 3
+    """
+    Esta funcion es para abrir una nueva ventana, que abre el mapa del nivel 3
+    """
+    nvl3 = tkinter.Tk()
+
+    
+def nivel4(): # Abrir la ventana del nivel 4, fusionar funcion de guardado a precionar nivel 4
+    """
+    Esta funcion es para abrir una nueva ventana, que abre el mapa del nivel 4
+    """
+    nvl4 = tkinter.Tk()
+
+
+    
+def nivel5(): # Abrir la ventana del nivel 5, fusionar funcion de guardado a precionar nivel 5
+    """
+    Esta funcion es para abrir una nueva ventana, que abre el mapa del nivel 4
+    """
+    nvl5 = tkinter.Tk()
+
+
+b1=tkinter.Button(w0,text="Nivel 1",font=("Arial",14),command=nivel1).place(x=400,y=10) #Boton nvl1
+b2=tkinter.Button(w0,text="Nivel 2",font=("Arial",14),command=nivel2).place(x=400,y=60) #Boton nvl2
+b3=tkinter.Button(w0,text="Nivel 3",font=("Arial",14),command=nivel3).place(x=400,y=110) #Boton nvl3
+b4=tkinter.Button(w0,text="Nivel 4",font=("Arial",14),command=nivel4).place(x=400,y=160)  #Boton nvl4
+b5=tkinter.Button(w0,text="Nivel 5",font=("Arial",14),command=nivel5).place(x=400,y=210) #Boton nvl5
+
+
+
+w0.mainloop()
+nvl1.mainloop()
+nvl2.mainloop()
+nvl3.mainloop()
+nvl4.mainloop()
+nvl5.mainloop()
